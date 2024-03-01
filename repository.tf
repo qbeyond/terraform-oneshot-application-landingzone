8406455ed83cbf7bf1b2b432808125dcd7115acb
Esteban Valverde <Esteban.Valverde@extern.qbeyond.de>
2024-03-01 14:07:10 +0100

    Change stage for local variable.

--- repository.tf
+++ repository.tf
@@ -38,9 +38,7 @@ resource "azuredevops_git_repository_file" "pipeline" {
 resource "azuredevops_git_repository_file" "main" {
   repository_id       = azuredevops_git_repository.landing_zone.id
   file                = "main.tf"
-  content             = templatefile("${path.module}/templates/main.tftpl", {
-    stage = var.stage
-  })
+  content             = templatefile("${path.module}/templates/main.tftpl", {})
   branch              = "refs/heads/${azuredevops_git_repository_branch.init.name}"
   commit_message      = "Add main.tf"
   overwrite_on_create = true
@@ -83,10 +81,7 @@ resource "azuredevops_git_repository_file" "vnet" {
   count         = var.vnet_config.address_space == null ? 0 : 1
   repository_id = azuredevops_git_repository.landing_zone.id
   file          = "vnet.tf"
-  content = templatefile("${path.module}/templates/vnet.tftpl", {
-    vnet_config = var.vnet_config
-    stage       = var.stage
-  })
+  content = templatefile("${path.module}/templates/vnet.tftpl", {})
   branch              = "refs/heads/${azuredevops_git_repository_branch.init.name}"
   commit_message      = "Add Vnet.tf"
   overwrite_on_create = true
@@ -100,9 +95,7 @@ resource "azuredevops_git_repository_file" "virtual_machine" {
   count         = var.create_virtual_machine_template == true ? 1 : 0
   repository_id = azuredevops_git_repository.landing_zone.id
   file          = "virtual_machine_template.tf"
-  content = templatefile("${path.module}/templates/virtual_machine.tftpl", {
-    stage = var.stage
-  })
+  content = templatefile("${path.module}/templates/virtual_machine.tftpl", {})
   branch              = "refs/heads/${azuredevops_git_repository_branch.init.name}"
   commit_message      = "Add virtual_machine_template.tf"
   overwrite_on_create = true
