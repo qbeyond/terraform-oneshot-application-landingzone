9c7bf00d8868ad49946861fb56bee56294d041d1
Esteban Valverde <esteban.valverde@qbeyond.de>
2025-01-28 19:23:13 +0100

    Fixed SQL variables.

--- variables.tf
+++ variables.tf
@@ -123,7 +123,7 @@ variable "sql" {
   type = object({
     create         = bool
     cust           = string
-    rg             = string      # Same name as vnet_config subnets key name and rg_config key.
+    rg_key         = string
     type           = string
     subnet         = string
     ip_server      = string
@@ -136,19 +136,31 @@ variable "sql" {
   description = "SQL configuration."
 
   validation {
+    condition     = var.sql.create == false || (var.sql.create == true && var.sql.cust != "" && var.sql.ip_server != "" && var.sql.database_name != "" && var.sql.collation != "")
+    error_message = "To create a SQL Server, may not be empty."
+  }
+  validation {
     condition     = var.sql.create == false || (var.sql.create == true && contains(["server", "managed"], var.sql.type))
     error_message = "The SQL type should be either `server` for SQL Server, or `managed` for SQL Managed Instance."
   }
   validation {
-    condition     = var.sql.type == "server" || (var.sql.type == "managed" && var.sql.sku_name != "ElasticPool")
+    condition     = var.sql.create == false || (var.sql.create == true && (var.sql.type == "server" || (var.sql.type == "managed" && var.sql.sku_name != "ElasticPool")))
     error_message = "Managed instances can not be ElasticPool sku."
   }
   validation {
-    condition     = contains(keys(var.sql.tags), "tagname") != true
+    condition     = var.sql.create == false || (var.sql.create == true && contains(keys(var.sql.tags), "tagname") != true)
     error_message = "The key `tagname` is just an example. Please remove it from the additional tags."
   }
   validation {
-    condition = var.sql.type == "managed" || (var.sql.type == "server" && contains([
+    condition     = var.sql.create == false || (var.sql.create == true && contains(keys(var.rg_config), var.sql.rg_key))
+    error_message = "Resource group key name must be defined as in rg_config variable"
+  }
+  validation {
+    condition     = var.sql.create == false || (var.sql.create == true && contains(keys(var.vnet_config.subnets), var.sql.subnet))
+    error_message = "Subnet key name must be defined as in vnet_config.subnets variable"
+  }
+  validation {
+    condition     = var.sql.create == false || (var.sql.create == true && (var.sql.type == "managed" || (var.sql.type == "server" && contains([
       "Free", "Basic", "ElasticPool", "S0", "S1", "S2", "S3", "S4", "S6", "S7", "S9", "S12", "P1", "P2", "P4", "P6", "P11", "P15", "DW100c", "DW200c",
       "DW300c", "DW400c", "DW500c", "DW1000c", "DW1500c", "DW2000c", "DW2500c", "DW3000c", "DW5000c", "DW6000c", "DW7500c", "DW10000c", "DW15000c", 
       "DW30000c", "DS100", "DS200", "DS300", "DS400", "DS500", "DS600", "DS1000", "DS1200", "DS1500", "DS2000", "GP_S_Gen5_1", "GP_Gen5_2", "GP_S_Gen5_2", 
@@ -165,13 +177,13 @@ variable "sql" {
       "HS_Gen5_16", "HS_S_Gen5_16", "HS_PRMS_16", "HS_MOPRMS_16", "HS_DC_16", "HS_Gen5_18", "HS_S_Gen5_18", "HS_PRMS_18", "HS_MOPRMS_18", "HS_DC_18", 
       "HS_Gen5_20", "HS_S_Gen5_20", "HS_PRMS_20", "HS_MOPRMS_20", "HS_DC_20", "HS_Gen5_24", "HS_S_Gen5_24", "HS_PRMS_24", "HS_MOPRMS_24", "HS_Gen5_32", 
       "HS_S_Gen5_32", "HS_PRMS_32", "HS_MOPRMS_32", "HS_DC_32", "HS_Gen5_40", "HS_S_Gen5_40", "HS_PRMS_40", "HS_MOPRMS_40", "HS_DC_40", "HS_PRMS_64", 
-      "HS_MOPRMS_64", "HS_Gen5_80", "HS_S_Gen5_80", "HS_PRMS_80", "HS_MOPRMS_80", "HS_PRMS_128"], var.sql.sku_name))
-    error_message = "The SKU name for SQL Server should be on of this: 'Free', 'Basic', 'ElasticPool', 'S0', 'S1', 'S2', 'S3', 'S4', 'S6', 'S7', 'S9', 'S12', 'P1', 'P2', 'P4', 'P6', 'P11', 'P15', 'DW100c', 'DW200c', 'DW300c', 'DW400c', 'DW500c', 'DW1000c', 'DW1500c', 'DW2000c', 'DW2500c', 'DW3000c', 'DW5000c', 'DW6000c', 'DW7500c', 'DW10000c', 'DW15000c', 'DW30000c', 'DS100', 'DS200', 'DS300', 'DS400', 'DS500', 'DS600', 'DS1000', 'DS1200', 'DS1500', 'DS2000', 'GP_S_Gen5_1', 'GP_Gen5_2', 'GP_S_Gen5_2', 'GP_DC_2', 'GP_Gen5_4', 'GP_S_Gen5_4', 'GP_DC_4', 'GP_Gen5_6', 'GP_S_Gen5_6', 'GP_DC_6', 'GP_Gen5_8', 'GP_S_Gen5_8', 'GP_DC_8', 'GP_Fsv2_8', 'GP_Gen5_10', 'GP_S_Gen5_10', 'GP_DC_10', 'GP_Fsv2_10', 'GP_Gen5_12', 'GP_S_Gen5_12', 'GP_DC_12', 'GP_Fsv2_12', 'GP_Gen5_14', 'GP_S_Gen5_14', 'GP_DC_14', 'GP_Fsv2_14', 'GP_Gen5_16', 'GP_S_Gen5_16', 'GP_DC_16', 'GP_Fsv2_16', 'GP_Gen5_18', 'GP_S_Gen5_18', 'GP_DC_18', 'GP_Fsv2_18', 'GP_Gen5_20', 'GP_S_Gen5_20', 'GP_DC_20', 'GP_Fsv2_20', 'GP_Gen5_24', 'GP_S_Gen5_24', 'GP_Fsv2_24', 'GP_Gen5_32', 'GP_S_Gen5_32', 'GP_DC_32', 'GP_Fsv2_32', 'GP_Fsv2_36', 'GP_Gen5_40', 'GP_S_Gen5_40', 'GP_DC_40', 'GP_Fsv2_72', 'GP_Gen5_80', 'GP_S_Gen5_80', 'GP_Gen5_128', 'BC_Gen5_2', 'BC_DC_2', 'BC_Gen5_4', 'BC_DC_4', 'BC_Gen5_6', 'BC_DC_6', 'BC_Gen5_8', 'BC_DC_8', 'BC_Gen5_10', 'BC_DC_10', 'BC_Gen5_12', 'BC_DC_12', 'BC_Gen5_14', 'BC_DC_14', 'BC_Gen5_16', 'BC_DC_16', 'BC_Gen5_18', 'BC_DC_18', 'BC_Gen5_20', 'BC_DC_20', 'BC_Gen5_24', 'BC_Gen5_32', 'BC_DC_32', 'BC_Gen5_40', 'BC_DC_40', 'BC_Gen5_80', 'BC_Gen5_128', 'HS_Gen5_2', 'HS_S_Gen5_2', 'HS_PRMS_2', 'HS_MOPRMS_2', 'HS_DC_2', 'HS_Gen5_4', 'HS_S_Gen5_4', 'HS_PRMS_4', 'HS_MOPRMS_4', 'HS_DC_4', 'HS_Gen5_6', 'HS_S_Gen5_6', 'HS_PRMS_6', 'HS_MOPRMS_6', 'HS_DC_6', 'HS_Gen5_8', 'HS_S_Gen5_8', 'HS_PRMS_8', 'HS_MOPRMS_8', 'HS_DC_8', 'HS_Gen5_10', 'HS_S_Gen5_10', 'HS_PRMS_10', 'HS_MOPRMS_10', 'HS_DC_10', 'HS_Gen5_12', 'HS_S_Gen5_12', 'HS_PRMS_12', 'HS_MOPRMS_12', 'HS_DC_12', 'HS_Gen5_14', 'HS_S_Gen5_14', 'HS_PRMS_14', 'HS_MOPRMS_14', 'HS_DC_14', 'HS_Gen5_16', 'HS_S_Gen5_16', 'HS_PRMS_16', 'HS_MOPRMS_16', 'HS_DC_16', 'HS_Gen5_18', 'HS_S_Gen5_18', 'HS_PRMS_18', 'HS_MOPRMS_18', 'HS_DC_18', 'HS_Gen5_20', 'HS_S_Gen5_20', 'HS_PRMS_20', 'HS_MOPRMS_20', 'HS_DC_20', 'HS_Gen5_24', 'HS_S_Gen5_24', 'HS_PRMS_24', 'HS_MOPRMS_24', 'HS_Gen5_32', 'HS_S_Gen5_32', 'HS_PRMS_32', 'HS_MOPRMS_32', 'HS_DC_32', 'HS_Gen5_40', 'HS_S_Gen5_40', 'HS_PRMS_40', 'HS_MOPRMS_40', 'HS_DC_40', 'HS_PRMS_64', 'HS_MOPRMS_64', 'HS_Gen5_80', 'HS_S_Gen5_80', 'HS_PRMS_80', 'HS_MOPRMS_80', 'HS_PRMS_128'"
+      "HS_MOPRMS_64", "HS_Gen5_80", "HS_S_Gen5_80", "HS_PRMS_80", "HS_MOPRMS_80", "HS_PRMS_128"], var.sql.sku_name))))
+    error_message = "SQL type should be 'managed' or 'server' and the SKU name for SQL Server should be on of this: 'Free', 'Basic', 'ElasticPool', 'S0', 'S1', 'S2', 'S3', 'S4', 'S6', 'S7', 'S9', 'S12', 'P1', 'P2', 'P4', 'P6', 'P11', 'P15', 'DW100c', 'DW200c', 'DW300c', 'DW400c', 'DW500c', 'DW1000c', 'DW1500c', 'DW2000c', 'DW2500c', 'DW3000c', 'DW5000c', 'DW6000c', 'DW7500c', 'DW10000c', 'DW15000c', 'DW30000c', 'DS100', 'DS200', 'DS300', 'DS400', 'DS500', 'DS600', 'DS1000', 'DS1200', 'DS1500', 'DS2000', 'GP_S_Gen5_1', 'GP_Gen5_2', 'GP_S_Gen5_2', 'GP_DC_2', 'GP_Gen5_4', 'GP_S_Gen5_4', 'GP_DC_4', 'GP_Gen5_6', 'GP_S_Gen5_6', 'GP_DC_6', 'GP_Gen5_8', 'GP_S_Gen5_8', 'GP_DC_8', 'GP_Fsv2_8', 'GP_Gen5_10', 'GP_S_Gen5_10', 'GP_DC_10', 'GP_Fsv2_10', 'GP_Gen5_12', 'GP_S_Gen5_12', 'GP_DC_12', 'GP_Fsv2_12', 'GP_Gen5_14', 'GP_S_Gen5_14', 'GP_DC_14', 'GP_Fsv2_14', 'GP_Gen5_16', 'GP_S_Gen5_16', 'GP_DC_16', 'GP_Fsv2_16', 'GP_Gen5_18', 'GP_S_Gen5_18', 'GP_DC_18', 'GP_Fsv2_18', 'GP_Gen5_20', 'GP_S_Gen5_20', 'GP_DC_20', 'GP_Fsv2_20', 'GP_Gen5_24', 'GP_S_Gen5_24', 'GP_Fsv2_24', 'GP_Gen5_32', 'GP_S_Gen5_32', 'GP_DC_32', 'GP_Fsv2_32', 'GP_Fsv2_36', 'GP_Gen5_40', 'GP_S_Gen5_40', 'GP_DC_40', 'GP_Fsv2_72', 'GP_Gen5_80', 'GP_S_Gen5_80', 'GP_Gen5_128', 'BC_Gen5_2', 'BC_DC_2', 'BC_Gen5_4', 'BC_DC_4', 'BC_Gen5_6', 'BC_DC_6', 'BC_Gen5_8', 'BC_DC_8', 'BC_Gen5_10', 'BC_DC_10', 'BC_Gen5_12', 'BC_DC_12', 'BC_Gen5_14', 'BC_DC_14', 'BC_Gen5_16', 'BC_DC_16', 'BC_Gen5_18', 'BC_DC_18', 'BC_Gen5_20', 'BC_DC_20', 'BC_Gen5_24', 'BC_Gen5_32', 'BC_DC_32', 'BC_Gen5_40', 'BC_DC_40', 'BC_Gen5_80', 'BC_Gen5_128', 'HS_Gen5_2', 'HS_S_Gen5_2', 'HS_PRMS_2', 'HS_MOPRMS_2', 'HS_DC_2', 'HS_Gen5_4', 'HS_S_Gen5_4', 'HS_PRMS_4', 'HS_MOPRMS_4', 'HS_DC_4', 'HS_Gen5_6', 'HS_S_Gen5_6', 'HS_PRMS_6', 'HS_MOPRMS_6', 'HS_DC_6', 'HS_Gen5_8', 'HS_S_Gen5_8', 'HS_PRMS_8', 'HS_MOPRMS_8', 'HS_DC_8', 'HS_Gen5_10', 'HS_S_Gen5_10', 'HS_PRMS_10', 'HS_MOPRMS_10', 'HS_DC_10', 'HS_Gen5_12', 'HS_S_Gen5_12', 'HS_PRMS_12', 'HS_MOPRMS_12', 'HS_DC_12', 'HS_Gen5_14', 'HS_S_Gen5_14', 'HS_PRMS_14', 'HS_MOPRMS_14', 'HS_DC_14', 'HS_Gen5_16', 'HS_S_Gen5_16', 'HS_PRMS_16', 'HS_MOPRMS_16', 'HS_DC_16', 'HS_Gen5_18', 'HS_S_Gen5_18', 'HS_PRMS_18', 'HS_MOPRMS_18', 'HS_DC_18', 'HS_Gen5_20', 'HS_S_Gen5_20', 'HS_PRMS_20', 'HS_MOPRMS_20', 'HS_DC_20', 'HS_Gen5_24', 'HS_S_Gen5_24', 'HS_PRMS_24', 'HS_MOPRMS_24', 'HS_Gen5_32', 'HS_S_Gen5_32', 'HS_PRMS_32', 'HS_MOPRMS_32', 'HS_DC_32', 'HS_Gen5_40', 'HS_S_Gen5_40', 'HS_PRMS_40', 'HS_MOPRMS_40', 'HS_DC_40', 'HS_PRMS_64', 'HS_MOPRMS_64', 'HS_Gen5_80', 'HS_S_Gen5_80', 'HS_PRMS_80', 'HS_MOPRMS_80', 'HS_PRMS_128'"
   }
   validation {
-    condition = var.sql.type == "server" || (var.sql.type == "managed" && contains([
-      "GP_Gen4", "GP_Gen5", "GP_Gen8IM", "GP_Gen8IH", "BC_Gen4", "BC_Gen5", "BC_Gen8IM", "BC_Gen8IH"], var.sql.sku_name))
-    error_message = "The SKU name for SQL Management Instances should be on of this: 'GP_Gen4', 'GP_Gen5', 'GP_Gen8IM', 'GP_Gen8IH', 'BC_Gen4', 'BC_Gen5', 'BC_Gen8IM', 'BC_Gen8IH'"
+    condition     = var.sql.create == false || (var.sql.create == true && (var.sql.type == "server" || (var.sql.type == "managed" && contains([
+      "GP_Gen4", "GP_Gen5", "GP_Gen8IM", "GP_Gen8IH", "BC_Gen4", "BC_Gen5", "BC_Gen8IM", "BC_Gen8IH"], var.sql.sku_name))))
+    error_message = "SQL type should be 'managed' or 'server' and the SKU name for SQL Management Instances should be on of this: 'GP_Gen4', 'GP_Gen5', 'GP_Gen8IM', 'GP_Gen8IH', 'BC_Gen4', 'BC_Gen5', 'BC_Gen8IM', 'BC_Gen8IH'"
   }
 }
 
