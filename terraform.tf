36b4a621f776632b8f4b8b64fa63a12ca7751f69
Esteban Valverde <esteban.valverde@qbeyond.de>
2025-07-21 09:56:54 +0200

    Fix version.

--- terraform.tf
+++ terraform.tf
@@ -2,19 +2,24 @@ terraform {
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = "~>0.4"
+      #version = "~>0.4"
+      version = ">=1.0.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~>2.36"
+      #version = "~>2.36"
+      version = "~> 2.39.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.117"
+      #version = "~>3.63"
+      #version = ">= 3.117"
+      version = ">= 4.0.0"
     }
     http-full = {
       source  = "salrashid123/http-full"
-      version = "~>1.3"
+      #version = "~>1.3"
+      version = "1.3.1"
     }
   }
 }
